f71cea6662ce255f85325a81be3ff122f36f9953
Pablo Ortega Mesa <portega@modyo.com>
2017-07-27 12:06:55 -0400

    Making optional configure AWS on application.yml

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,6 +1,8 @@
 require 'aws-sdk'
 
-Aws.config.update({
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-})
+if ENV['ENABLE_AWS'] == 'true'
+  Aws.config.update({
+    region: ENV['AWS_REGION'],
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  })
+end
