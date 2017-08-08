bd344554e819132cf834443a7112974967590694
Pablo Ortega Mesa <portega@modyo.com>
2017-08-08 19:54:57 -0400

    Configuring timezone

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Demo
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = ENV['QUEUE_PROVIDER'].to_sym
+
+    config.time_zone = 'Santiago'
   end
 end
